83511743080f59ed59a6fe34fc0d8ec6a17b1bf5
TranTam31 <tranductamhoctap@gmail.com>
2025-06-08 22:55:18 +0700

    Cutting KitchenObject SO

--- Assets/Scripts/BaseCounter.cs
+++ Assets/Scripts/BaseCounter.cs
@@ -15,6 +15,10 @@ public class BaseCounter : MonoBehaviour, IKitchenObjectParent
         Debug.LogError("BaseCounter interact");
     }
 
+    public virtual void InteractAlternate(Player player) {
+        Debug.LogError("BaseCounter interact");
+    }
+
     public Transform GetKitchenObjectFollowTransform() {
         return counterTopPoint;
     }
